c7465c9f5326982769283b14e79b072f7f0ebe29
fjgonmir <fjgonmir24@gmail.com>
2021-11-21 19:15:47 +0100

    method chain different method and obtain parameter with query

--- myApp.js
+++ myApp.js
@@ -42,4 +42,19 @@ app.get("/:word/echo",function(req,res){
     })
 })
 
+app.route("/name")
+.get(function(req,res){
+    firstName = req.query.first;
+    lastName = req.query.last;
+    res.json({
+        name : `${firstName} ${lastName}`
+    })
+}).post(function(req,res){
+    firstName = req.query.first;
+    lastName = req.query.last;
+    res.json({
+        name : `${firstName} ${lastName}`
+    }) 
+})
+
 module.exports = app;
