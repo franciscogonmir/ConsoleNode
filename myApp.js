6e6e931f951ac8b4678806f9324fa6ab5295206f
fjgonmir <fjgonmir24@gmail.com>
2021-11-21 01:47:26 +0100

    add endpoint json

--- myApp.js
+++ myApp.js
@@ -13,6 +13,12 @@ app.get("/",function(req,res){
     res.sendFile(__dirname + "/views/index.html");
 })
 
+app.get("/json",function(req,res){
+    res.json({
+        "message":"Hello json"
+    })
+})
+
 
 
 
