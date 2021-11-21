7a59bccb4a900235ccd8fc9549565e8ab121c48a
fjgonmir <fjgonmir24@gmail.com>
2021-11-21 18:30:54 +0100

    middelware chain in method

--- myApp.js
+++ myApp.js
@@ -28,5 +28,12 @@ app.get("/json",function(req,res){
         "message":'Hello json'  
     })
 })
+//middleware chain for method
+app.get("/now",function(req,res,next){
+    req.time = new Date().toString();
+    next()
+},function(req,res){
+    res.send({time:req.time})
+})
 
 module.exports = app;
