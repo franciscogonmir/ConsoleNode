b880c1541bc105be033701f6b6d559c468f38fb4
fjgonmir <fjgonmir24@gmail.com>
2021-11-21 01:41:57 +0100

    add middleware to serve static style files

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.use("/public",express.static(__dirname + "/public"));
+
 
 console.log("Hello world");
 
@@ -10,6 +12,7 @@ console.log("Hello world");
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/views/index.html");
 })
+
 
 
 
